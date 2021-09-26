5ed1b4116d1b2b0f8450c5a56e62bbd63e7facf7
Harsh Popat <harshpopat75@gmail.com>
2021-09-26 20:36:10 +0530

    added more users and organized the usercards using grid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from "./components/layout/Navbar"
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 
 class App extends Component {
 
+// below all the user stuff is added into a container class (ps what is not part of navbr is added to container)
 
   render() {
     return (
       <div>
         <Navbar />
-        <UserItem />
+        <div className="container">   
+          <Users />
+        </div>
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
+import Users from './Users'
 
 class UserItem extends Component {
-  // constructor() {
-  //   super();
-
-  //   this.
-      state = {   //we can do this without using constructor in this way
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
+  
+      // state = {   //we can do this without using constructor in this way
+      // id: 'id',
+      // login: 'mojombo',
+      // avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+      // html_url: 'https://github.com/mojombo'
     //}
-  }// here we have hardcoded our data from api.github.com/users, futher we will directly fetch data
+  
+  //};
+  
+  // here we have hardcoded our data from api.github.com/users, futher we will directly fetch data
 
 
   render() {
-    const {avatar_url, login, html_url} = this.state  // destructuring
+
+    const {avatar_url, login, html_url} = this.props.user ;
+    
+    // destructuring, instead of state we are using props which we passed and we passed it into user, therefore this.props.user.anything
+    
     return (
       <div className="card text-center">
         <img src={avatar_url}  // because of destructuring here we do not need to use this.state
