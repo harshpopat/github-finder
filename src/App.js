3684b9ca0e90b654ecee8255333d0fe48ea5ad16
Harsh Popat <harshpopat75@gmail.com>
2021-09-27 18:04:31 +0530

    fetched the data from api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import Navbar from "./components/layout/Navbar"
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 
 class App extends Component {
 
+  state = {
+    users: [],
+    loading: false
+  }
+
 // below all the user stuff is added into a container class (ps what is not part of navbr is added to container)
+
+// a lifecycle method, we will make an http req and fetch data from g8ithub as soon as the component mounts
+
+//axios deals with promises thats why .then, we will refactor it to async and await
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ loading: false, users: res.data })
+  }
 
   render() {
     return (
       <div>
         <Navbar />
         <div className="container">   
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,36 +3,11 @@ import UserItem from './UserItem'
 
 class Users extends Component {
 
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-
-      {
-        id: 'id',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett'
-      }
-    ]
-  }
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map( user => (
-          <UserItem key={user.id} user = {user} />
+        {this.props.users.map( user => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
