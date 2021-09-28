ea12605546725388a21f639ac1760afea5b8d36b
Harsh Popat <harshpopat75@gmail.com>
2021-09-28 19:08:15 +0530

    added the page for single user component data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from "./components/layout/Navbar"
+import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
-import About from './components/pages/About'
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
-
 class App extends Component {
-
   state = {
     users: [],
+    user: {},
     loading: false,
-    alert: null
-  }
+    alert: null,
+  };
 
-// below all the user stuff is added into a container class (ps what is not part of navbr is added to container)
+  // below all the user stuff is added into a container class (ps what is not part of navbr is added to container)
 
-// a lifecycle method, we will make an http req and fetch data from g8ithub as soon as the component mounts
+  // a lifecycle method, we will make an http req and fetch data from g8ithub as soon as the component mounts
 
-//axios deals with promises thats why .then, we will refactor it to async and await
+  //axios deals with promises thats why .then, we will refactor it to async and await
 
   // async componentDidMount() {
   //   this.setState({ loading: true })
@@ -32,59 +32,86 @@ class App extends Component {
   // }
 
   //Search github users, this is not the effective way to perform search as we have passed props up instead of down, its called props drilling, but we will refactor it using redux, context api and make the code more efficient
-  
-  searchUsers = async text => {
 
-    this.setState({ loading: true })
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    this.setState({ users: res.data.items, loading: false  })
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Get single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
 
   // Clear users from state
-  clearUsers = () => {this.setState({ users: [], loading: false })}
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   // Set Alert
   setAlert = (msg, type) => {
-    this.setState({ alert: {msg: msg, type: type} })  
-    
+    this.setState({ alert: { msg: msg, type: type } });
+
     //here both are equal lhs and rhs so we can also write it as alert{msg, type}
 
     setTimeout(() => this.setState({ alert: null }), 5000);
-  }
-
+  };
 
   render() {
-    const { users, loading } = this.state
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
-      <div>
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                  <Search 
-                    searchUsers={this.searchUsers} 
-                    clearUsers={this.clearUsers}
-                    showClear={users.length ? true : false} 
-                    setAlert={this.setAlert}
+        <div>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
                   />
-                  <Users loading={loading} users={users}/>
-              </Fragment>
-            )} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-          
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
       </Router>
     );
   }
-
 }
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,42 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 //as user is a prop passed here so we need to use prop type, use impt shortcut
 
-const UserItem = ({user: {avatar_url, login, html_url}}) => {  
-  
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
   //destructuring the props here itself, we are fetching data from user prop which is passed
-   
+
   // here we have hardcoded our data from api.github.com/users, futher we will directly fetch data
-  
+
   // destructuring, instead of state we are using props which we passed and we passed it into user, therefore this.props.user.anything
-    
-    return (
-      <div className="card text-center">
-        <img src={avatar_url}  // because of destructuring here we do not need  to use this.state
-        alt=""
-        className="round-img" 
+
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url} // because of destructuring here we do not need  to use this.state
+        alt=''
+        className='round-img'
         style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
+      />
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
       </div>
-
-    )
-  
-}
-
-UserItem.propTypes = {
- 
-  //here user (prop which is passed) is an object so
-  //use ptor shortcut for rhs
-  user: PropTypes.object.isRequired
- 
+    </div>
+  );
 };
 
-export default UserItem
+UserItem.propTypes = {
+  //here user (prop which is passed) is an object so
+  //use ptor shortcut for rhs
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
