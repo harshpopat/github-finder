bea1623b00e768b11fd33b51f99a6dcf2f5db0e4
Harsh Popat <harshpopat75@gmail.com>
2021-09-28 23:34:12 +0530

    added the repos section in the page and completed the app

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import Spinner from '../layout/Spinner';
+import Repos from '../Repos/Repos';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+  };
 
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
+      blog,
       login,
       html_url,
       followers,
@@ -20,9 +35,78 @@ class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit GitHub Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username</strong> {login}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website</strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
+      </Fragment>
+    );
   }
 }
 
